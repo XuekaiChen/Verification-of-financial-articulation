1e41736db716ec6ea1a0b904ca1b9767510655ec
XuekaiChen <50224248+XuekaiChen@users.noreply.github.com>
2023-03-09 10:34:35 +0800

    Add files via upload

--- data2json.py
+++ data2json.py
@@ -1,6 +1,5 @@
+import math
 from functools import reduce
-import pandas as pd
-import os
 import json
 from util import is_number, is_number_list
 
@@ -8,7 +7,12 @@ from util import is_number, is_number_list
 def getTitle(inloc, Elecount, list1, row):
     title_list = []
     for j in range(inloc + 1, len(list1[0])):
-        if list1[0][j] == "":
+        if list1[0][inloc+1] is None:
+            list1[0][inloc+1] = ""
+        if isinstance(list1[0][j], float):
+            if math.isnan(list1[0][j]):
+                list1[0][j] = ""
+        if list1[0][j] == "" or list1[0][j] is None:
             list1[0][j] = list1[0][j - 1]
 
     for j in range(inloc + 1, len(row)):
@@ -20,31 +24,26 @@ def getTitle(inloc, Elecount, list1, row):
     return title_list
 
 
-def excels2json(excel_folder, out_json):
+def excels2json(table_dict, out_json=False):
     # 将所有表格以行名为索引记录到字典里，输出json文件
-    excelall = os.listdir(excel_folder)
     data_all = {}
-    for excel in excelall:
+    for table_name, table in table_dict.items():
         data_excel = {}
-        xlsx = pd.ExcelFile(os.path.join(excel_folder, excel))
-        sheet1 = pd.read_excel(xlsx, 'Sheet1', header=None, keep_default_na=False)
-        list1 = sheet1.values.tolist()  # 以行为元素的列表
         a = -1
         Elecount = 0
         inlocList = []
-
         flag = True
-        for row in list1:  # 格式处理
+        for row in table:  # 格式处理
             a = a + 1
             if a == 0:
                 title = str(row[0])
                 continue
             else:
-                if row[0] == "":
-                    row[0] = list1[a - 1][0]
+                if row[0] == "" or row[0] is None:
+                    row[0] = table[a - 1][0]
                 if flag:
-                    if str(row[0]) == title and a + 1 < len(list1) and str(list1[a + 1][0]) != title and str(
-                            list1[a - 1][0]) == title:
+                    if str(row[0]) == title and a + 1 < len(table) and str(table[a + 1][0]) != title and str(
+                            table[a - 1][0]) == title:
                         Elecount = Elecount + 1
                     else:
                         flag = False
@@ -54,39 +53,48 @@ def excels2json(excel_folder, out_json):
                 row[0] = row[0].replace('\n', '')
             j = len(row) - 1
             while j > 0:
+                if row[j] is None:
+                    row[j] = str("")
                 if type(row[j]) == str:
                     row[j] = row[j].replace(',', '')
                 if row[j] == '未披露':
                     row[j] = float(-1)
                 if row[j] == '-':
                     row[j] = 0
-                if (is_number(row[j])):
+                if is_number(row[j]):
                     row[j] = float(row[j])
+                    if math.isnan(row[j]):
+                        row[j] = ""
+                        continue
                 j = j - 1
             inloc = 0
-            while inloc + 1 < len(row) and row[inloc + 1] != '' and row[inloc + 1] != '-' and type(row[inloc + 1]) == str:
+            while inloc + 1 < len(row) and row[inloc + 1] != '' and row[inloc + 1] != '-' and type(
+                    row[inloc + 1]) == str:
                 inloc = inloc + 1
             if inloc == len(row) - 1:
                 continue
             if inloc > 0:
                 row[inloc] = reduce(lambda x, y: str(x) + str(y), row[0:inloc + 1])
-            row[inloc] = row[inloc].replace('\n', '')
+                row[inloc] = row[inloc].replace('\n', '')
             if isinstance(row[inloc], str) and is_number_list(row[inloc + 1:len(row)]):
                 data_excel[row[inloc]] = row[inloc + 1:len(row)]
                 inlocList.append(inloc)
 
         if data_excel:
-            data_excel['title'] = getTitle(min(inlocList), Elecount, list1, row)
-            data_all[excel] = data_excel
+            data_excel['title'] = getTitle(min(inlocList), Elecount, table, row)
+            data_all[table_name] = data_excel
 
-    with open(out_json, 'w+', encoding='utf-8') as fp:
-        json.dump(data_all, fp, indent=2, ensure_ascii=False)
+    if out_json:
+        with open(out_json, 'w+', encoding='utf-8') as fp:
+            json.dump(data_all, fp, indent=2, ensure_ascii=False)
+
+    return data_all
 
 
 if __name__ == "__main__":
-    tables_folder = "tables"
-    out_json_path = "data_all_v2.json"
-    excels2json(tables_folder, out_json_path)
-
-
-
+    table_dict_path = "table_dict.json"
+    with open(table_dict_path, 'r', encoding='utf8') as fp:
+        table_dict = json.load(fp)
+    out_json_path = "table_content_sample.json"
+    json_data = excels2json(table_dict, out_json_path)
+    print(json_data)
